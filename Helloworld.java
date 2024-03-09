0af87e3501402295190befa2b62b7aca1b4e94a8
Myo Thiha Kyaw <myothihakyaw571@gmail.com>
2024-03-09 13:37:45 +0630

    updated

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
 	           public static void main(String[] args){
                                 System.out.print("Hello World");
+                                System.out.print("second changes");
 long x=100000000L;
 int y=int(x);
 }
